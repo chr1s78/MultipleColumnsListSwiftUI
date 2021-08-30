92161277989a48e404c629856b8febbb02c71739
chr1s78 <ayahirano2014@126.com>
2021-08-30 18:49:24 +0800

    Initial Commit

---
+++ MultipleColumnsListSwiftUI/MultipleColumnsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleColumnsListSwiftUIApp.swift
+//  MultipleColumnsListSwiftUI
+//
+//  Created by Chr1s on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct MultipleColumnsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
